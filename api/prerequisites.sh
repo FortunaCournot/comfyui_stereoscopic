b274f393b39cd1e709d3a714a1960eb1d9e15061
FortunaCournot <65296393+FortunaCournot@users.noreply.github.com>
2025-10-04 16:42:58 +0200

    speed-up daemon start, adding no-sbs placeholder task

--- api/prerequisites.sh
+++ api/prerequisites.sh
@@ -449,12 +449,17 @@ if [ -e "custom_nodes/comfyui_stereoscopic/.test/.install" ] ; then
 	fi
 fi
 
-# Do initial auto-forward
+# Do initial auto-forward and cleanup
 if [ $PIPELINE_AUTOFORWARD -ge 1 ] ; then
-	echo -e $"\e[2mSearching for files left to forward\e[0m"
+	echo -e $"\e[2mSearching for files left to forward and cleanup.\e[0m"
 	for stagepath in scaling slides fullsbs singleloop slideshow concat dubbing/sfx watermark/encrypt watermark/decrypt caption interpolate ; do
 		[ $loglevel -ge 1 ] && echo " - $stagepath"
-		./custom_nodes/comfyui_stereoscopic/api/forward.sh $stagepath || exit 1
+		FILECOUNT=`find output/vr/$stagepath -maxdepth 1 -type f -name '*.*' | wc -l 2>/dev/null` 
+		# forward.txt + one media
+		if [ $FILECOUNT -gt 1 ] ; then
+			./custom_nodes/comfyui_stereoscopic/api/forward.sh $stagepath || exit 1
+		fi
+		rm -rf -- output/vr/$stagepath/intermediate input/vr/$stagepath/intermediate 2>/dev/null
 	done
 	
 	TASKDIR=`find output/vr/tasks -maxdepth 1 -type d`
@@ -462,7 +467,12 @@ if [ $PIPELINE_AUTOFORWARD -ge 1 ] ; then
 		task=${task#output/vr/tasks/}
 		if [ ! -z $task ] ; then
 			[ $loglevel -ge 1 ] && echo " - tasks/$task"
-			./custom_nodes/comfyui_stereoscopic/api/forward.sh tasks/$task || exit 1
+			FILECOUNT=`find output/vr/$stagepath -maxdepth 1 -type f -name '*.*' | wc -l 2>/dev/null`  
+			# forward.txt + one media
+			if [ $FILECOUNT -gt 1 ] ; then
+				./custom_nodes/comfyui_stereoscopic/api/forward.sh tasks/$task || exit 1
+			fi
+			rm -rf -- output/vr/$stagepath/intermediate input/vr/$stagepath/intermediate 2>/dev/null
 		fi
 	done
 else
