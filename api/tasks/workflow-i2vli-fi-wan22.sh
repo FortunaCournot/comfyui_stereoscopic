45b2801917f2bb10785f56d8f8d1846b952748fc
FortunaCournot <65296393+FortunaCournot@users.noreply.github.com>
2026-02-28 10:55:31 +0100

    Update workflow-i2vli-fi-wan22.sh

--- api/tasks/workflow-i2vli-fi-wan22.sh
+++ api/tasks/workflow-i2vli-fi-wan22.sh
@@ -191,7 +191,7 @@ else
     if [[ "$timeout" =~ ^[0-9]+$ ]]; then
       if [[ "$secs" -gt "$timeout" ]]; then
         echo "timeout reached. ( $timeout ). Restarting ComfyUI..."
-        cmd='Get-Process -Name python | Where-Object { ($_.PagedMemorySize64/1KB) -gt 20000000 } | ForEach-Object { Write-Output ("Killing {0} (pid {1}) PM(K)={2}" -f $_.ProcessName,$_.Id,[math]::Round($_.PagedMemorySize64/1KB)); Stop-Process -Id $_.Id -Force -ErrorAction SilentlyContinue }'
+        cmd='Get-Process -Name python | Where-Object { ($_.PagedMemorySize64/1KB) -gt 10000000 } | ForEach-Object { Write-Output ("Killing {0} (pid {1}) PM(K)={2}" -f $_.ProcessName,$_.Id,[math]::Round($_.PagedMemorySize64/1KB)); Stop-Process -Id $_.Id -Force -ErrorAction SilentlyContinue }'
         encoded=$(printf '%s' "$cmd" | iconv -f utf-8 -t utf-16le | base64 -w0)
         powershell.exe -NoProfile -EncodedCommand "$encoded"
         echo "Waiting for restart."
